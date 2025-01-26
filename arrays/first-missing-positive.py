80268689fd738011931707eb0b0e4ab1521c9d72
Rishabh-Hupr <67226529+Rishabh-Hupr@users.noreply.github.com>
2025-01-26 22:16:35 +0000

    first-missing-positive

---
+++ arrays/first-missing-positive.py
@@ -0,0 +1,48 @@
+import heapq as hq
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        # hq.heapify(nums)
+        # print(nums)
+        # ans = 1
+        # n = 0
+        # for i in nums:
+        #     n += 1
+        #     if i==ans:
+        #         ans += 1
+        # for i in range(n-1, -1, -1):
+        #     if nums[i] == ans:
+        #         ans += 1
+        # return ans
+        # j = n - 1
+        # i = 0
+        # while i <= j:
+        #     if nums[i] <= 0:
+        #         nums[i], nums[j] = nums[j], nums[i]
+        #         nums.pop()
+        #         j -= 1
+        #     i += 1
+
+        '''
+            Time: O(n)
+            Space: O(1)
+            DESCRIPTION
+                We do a cyclic sort algorithm, placing numbers in the arrays
+                length range on the respective indexes
+        '''
+
+        n = len(nums)
+        i = 0
+        while i < n:
+            val = nums[i]
+            if 1<= val <= n:
+                if val != i+1 and nums[val - 1] != nums[i]:
+                    nums[val-1], nums[i] = val, nums[val-1]
+                    continue
+            i += 1
+        for i in range(n):
+            if nums[i] != i+1:
+                return i+1
+        return n+1
+        # hq.heapify(nums)
+        # print(nums)
