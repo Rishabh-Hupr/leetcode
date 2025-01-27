79b502d746c41cb325844008c3eae338a3a6ca72
Rishabh-Hupr <67226529+Rishabh-Hupr@users.noreply.github.com>
2025-01-27 01:00:03 +0000

    trapping-rain-water

---
+++ 2pointers/trapping-rain-water.py
@@ -0,0 +1,24 @@
+class Solution:
+    def trap(self, arr: List[int]) -> int:
+        '''
+            Time: O(n) + O(n) + O(n)
+            Space: O(n) + O(n)
+        '''
+        n = len(arr)
+        l = [0] * n
+        maxl = arr[0]
+        r = [0] * n
+        maxr = arr[n-1]
+        ans = 0
+        for i in range(1, n):
+            l[i] = maxl
+            maxl = max(maxl, arr[i])
+        
+        for i in range(n-2, -1, -1):
+            r[i] = maxr
+            maxr = max(maxr, arr[i])
+        
+        for i in range(n):
+            if min(l[i], r[i]) > arr[i]:
+                ans += min(l[i], r[i]) - arr[i]
+        return ans
